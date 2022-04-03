63082b3aa572f1bdf3667477ca5d01276b4411e8
circular-biscuit <ishandeb@live.com>
2022-04-04 00:26:45 +0530

    recipes output on cards on home page

---
+++ src/components/RecipeList.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+//styles
+import './recipeList.css'
+
+export default function RecipeList({ recipes }) {
+  return (
+    <div className='recipe-list'>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="card">
+          <h3>{recipe.title}</h3>
+          <p>Cooking Time: {recipe.cookingTime}</p>
+          <div>{recipe.method.substring(0,100)}...</div>
+          <Link to={`/recipes/${recipe.id}`}>Cook Me</Link>
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,13 +9,13 @@ import './home.css'
 
 export default function Home() {
 
-  const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+  const { data: recipes, isPending, error } = useFetch('http://localhost:3000/recipes')
   
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && <RecipeList />}
+      {recipes && <RecipeList recipes={recipes}/>}
     </div>
   )
 }
