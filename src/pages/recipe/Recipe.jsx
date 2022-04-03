18d5c84613c9a7359d73457c961425620a0c8111
circular-biscuit <ishandeb@live.com>
2022-04-04 00:54:42 +0530

    full recipe output on individual pages

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,7 +1,28 @@
+import { useFetch } from '../../hooks/useFetch'
+import { useParams, Link } from 'react-router-dom'
+
+//styles
 import './recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { data:recipe, isPending, error} = useFetch(url)
+
   return (
-    <div>Recipe</div>
+    <div>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && 
+        <div className="recipe">
+          <h1>{recipe.title}</h1>
+          <p>Cooking Time: {recipe.cookingTime}</p>
+          {recipe.ingredients.map(ingredient => (
+            <p>- {ingredient}</p>
+          ))}
+          <div>{recipe.method}</div>
+        </div>
+      }
+    </div>
   )
 }
