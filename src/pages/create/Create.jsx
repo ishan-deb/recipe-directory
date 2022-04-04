c345a28919f9ac1fedc71fe25a4eb9da0168a7f8
circular-biscuit <ishandeb@live.com>
2022-04-04 05:55:02 +0530

    add new recipes to db.json

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react'
-
+import { useFetch } from '../../hooks/useFetch'
 
 //styles
 
@@ -13,9 +13,12 @@ export default function Create() {
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
 
+  const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
+
   const handleSubmit = e => {
     e.preventDefault()
     console.log(title, method, cookingTime, ingredients)
+    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes'})
 
 
     formReset()
