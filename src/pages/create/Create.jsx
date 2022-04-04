8b943edbbc27b93115e97ee33d90f32c6c9f2525
circular-biscuit <ishandeb@live.com>
2022-04-04 06:04:42 +0530

    redirect to home page on recipe submit

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,4 +1,5 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import { useFetch } from '../../hooks/useFetch'
 
 //styles
@@ -12,17 +13,21 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
+  const history = useHistory()
 
   const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
 
   const handleSubmit = e => {
     e.preventDefault()
-    console.log(title, method, cookingTime, ingredients)
     postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes'})
-
-
     formReset()
   }
+
+  useEffect(()=> {
+    if (data) {
+      history.push('/')
+    }
+  },[data])
 
   const formReset = () => {
     setTitle('')
